f5708e67bb499bab557cbf96dd30ae8ab262543c
cuongnguyen71190 <cuongnguyen071190@gmail.com>
2017-11-30 09:04:59 +0700

    end lesson 28

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -4,18 +4,15 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\User; 
+use App\Activity;
 
 class ProfileController extends Controller
 {
     public function show(User $user)
     {
-    	$activities = $user->activity()->latest()->with('subject')->get()->groupBy(function ($activity) {
-    		return $activity->created_at->format('Y-m-d');
-    	});
-    	
     	return view('profiles.show', [
     		'profileUser' => $user,
-    		'activities' => $activities
+    		'activities' => Activity::feed($user)
     	]);
     }
 }
